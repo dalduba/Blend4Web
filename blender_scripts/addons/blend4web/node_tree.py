8ad0cd7ab1052ea527943ed66560ddff158e82a3
Dal Duba <dalduba@gmail.com>
2015-03-02 21:59:02 +0300

    target start implementation

--- blender_scripts/addons/blend4web/node_tree.py
+++ blender_scripts/addons/blend4web/node_tree.py
@@ -115,92 +115,79 @@ class MyCustomNode(Node, B4WLogicNode):
         return "I am a custom node"
 
 class SensorSocket(NodeSocket):
-    # Description string
-    '''Custom node socket type'''
-    # Optional identifier string. If not explicitly defined, the python class name is used.
     bl_idname = 'SensorSocketType'
-    # Label for nice name display
     bl_label = 'Sensor Node Socket'
-
-    # Enum items list
     my_items = [
         ("Init", "Init", "Fire at Initialisation time"),
         ("Always", "Always", "Fire every frame"),
         ("Bounding begin", "Bounding begin", "---"),
         ("Bounding end", "Bounding end", "---"),
     ]
-
     myEnumProperty = bpy.props.EnumProperty(name="SensorType", description="Sensor Type", items=my_items, default='Init')
 
-    # Optional function for drawing the socket input value
     def draw(self, context, layout, node, text):
         if self.is_output or self.is_linked:
             layout.prop(self, "myEnumProperty", text=text)
         else:
             layout.label(text)
 
-    # Socket color
     def draw_color(self, context, node):
         return (1.0, 0.4, 0.216, 0.5)
 
 
-
-# Derived from the Node base type.
 class SensorNode(Node, B4WLogicNode):
-    # === Basics ===
-    # Description string
-    '''A custom node'''
-    # Optional identifier string. If not explicitly defined, the python class name is used.
     bl_idname = 'SensorNode'
-    # Label for nice name display
     bl_label = 'Sensor'
-    # Icon identifier
-    # bl_icon = 'SOUND'
 
-    # === Custom Properties ===
-    # These work just like custom properties in ID data blocks
-    # Extensive information can be found under
-    # http://wiki.blender.org/index.php/Doc:2.6/Manual/Extensions/Python/Properties
-    # myStringProperty = bpy.props.StringProperty()
-    # myFloatProperty = bpy.props.FloatProperty(default=3.1415926)
-
-    # === Optional Functions ===
-    # Initialization function, called when a new node is created.
-    # This is the most common place to create the sockets for a node, as shown below.
-    # NOTE: this is not the same as the standard __init__ function in Python, which is
-    #       a purely internal Python method and unknown to the node system!
     def init(self, context):
-        # self.inputs.new('CustomSocketType', "Hello")
-        # self.inputs.new('NodeSocketFloat', "World")
-        # self.inputs.new('NodeSocketVector', "!")
-
         self.outputs.new('SensorSocketType', "")
 
-    # Copy function to initialize a copied node from an existing one.
     def copy(self, node):
         print("Copying from node ", node)
 
-    # Free function to clean up on removal.
-    def free(self):
-        print("Removing node ", self, ", Goodbye!")
-
-    # Additional buttons displayed on the node.
     def draw_buttons(self, context, layout):
         layout.label("Node settings")
-        layout.prop(self, "myFloatProperty")
 
-    # Detail buttons in the sidebar.
-    # If this function is not defined, the draw_buttons function is used instead
-    def draw_buttons_ext(self, context, layout):
-        layout.prop(self, "myFloatProperty")
-        # myStringProperty button will only be visible in the sidebar
-        layout.prop(self, "myStringProperty")
-
-    # Optional: custom label
-    # Explicit user label overrides this, but here we can define a label dynamically
     def draw_label(self):
-        return "I am a custom node"
+        return "Sensor node"
 
+#----------------------
+class TargetSocket(NodeSocket):
+    bl_idname = 'TargetSocketType'
+    bl_label = 'Target Node Socket'
+    my_items = []
+    ObjectsProperty = 0
+    def init(self, context):
+        my_items = bpy.data.objects
+        ObjectsProperty = bpy.props.PointerProperty(name="TargetType", description="Target Type", items=my_items)
+
+    def draw(self, context, layout, node, text):
+        if self.is_output or self.is_linked:
+            # layout.prop(self, "ObjectsProperty", text=text)
+            pass
+        else:
+            layout.label(text)
+
+    def draw_color(self, context, node):
+        return (0.0, 1.0, 0.216, 0.5)
+
+
+class TargetNode(Node, B4WLogicNode):
+    bl_idname = 'TargetNode'
+    bl_label = 'Target'
+
+    def init(self, context):
+        self.outputs.new('TargetSocketType', "")
+
+    def copy(self, node):
+        print("Copying from node ", node)
+
+    def draw_buttons(self, context, layout):
+        layout.label("Node settings")
+
+    def draw_label(self):
+        return "Target node"
+#--------------------------------
 
 ### Node Categories ###
 # Node categories are a python system for automatically
@@ -216,8 +203,7 @@ from nodeitems_utils import NodeCategory, NodeItem
 class MyNodeCategory(NodeCategory):
     @classmethod
     def poll(cls, context):
-        return True
-        #context.space_data.tree_type == 'B4WLogicNodeTreeType'
+        return context.space_data.tree_type == 'B4WLogicNodeTreeType'
 
 # all categories in a list
 node_categories = [
@@ -230,6 +216,7 @@ node_categories = [
             }),
         ]),
     MyNodeCategory("Targets", "Targets", items=[
+        NodeItem("TargetNode", label="Target",),
         # the node item can have additional settings,
         # which are applied to new nodes
         # NB: settings values are stored as string expressions,
@@ -256,6 +243,8 @@ def register():
     bpy.utils.register_class(SensorSocket)
     bpy.utils.register_class(MyCustomNode)
     bpy.utils.register_class(SensorNode)
+    bpy.utils.register_class(TargetNode)
+    bpy.utils.register_class(TargetSocket)
 
     nodeitems_utils.register_node_categories("CUSTOM_NODES", node_categories)
 
@@ -267,6 +256,8 @@ def unregister():
     bpy.utils.unregister_class(MyCustomSocket)
     bpy.utils.unregister_class(SensorSocket)
     bpy.utils.unregister_class(MyCustomNode)
+    bpy.utils.unregister_class(TargetNode)
+    bpy.utils.unregister_class(TargetSocket)
 
 
 if __name__ == "__main__":
